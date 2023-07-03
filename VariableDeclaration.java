33176cc0a592b052ee1694394e67ea6ea02ce31d
bhagyasriy <bhagyasvec@gmail.com>
2023-07-03 11:09:33 +0530

    Create VariableDeclaration.java
    VariableDeclaration

---
+++ VariableDeclaration.java
@@ -0,0 +1,17 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        // Integer variable
+        int age = 25;
+
+        // Double variable
+        double weight = 68.5;
+
+        // String variable
+        String name = "John Doe";
+
+        // Printing the values of the variables
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+        System.out.println("Name: " + name);
+    }
+}
